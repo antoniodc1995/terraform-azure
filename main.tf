c618f3dfe2c612a5f34458e46056f2da821aba6a
antoniodc1995 <a.dececio1995@gmail.com>
2024-07-25 23:10:05 +0200

    modified:   .gitignore new file:   linux-ssh-script.tpl modified:   main.tf

--- main.tf
+++ main.tf
@@ -95,18 +95,18 @@ resource "azurerm_network_interface" "mtc-nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "mtc-vm" {
-  name                = "mtc-vm"
-  resource_group_name = azurerm_resource_group.mtc-rg.name
-  location            = azurerm_resource_group.mtc-rg.location
-  size                = "Standard_B1s"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.mtc-nic.id
-  ]
+  name                  = "mtc-vm"
+  resource_group_name   = azurerm_resource_group.mtc-rg.name
+  location              = azurerm_resource_group.mtc-rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.mtc-nic.id]
+
+  custom_data = filebase64("customdata.tpl")
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("/home/antoniodc95/.ssh/mtcazurekey.pub")
+    public_key = file("~/.ssh/mtcazurekey.pub")
   }
 
   os_disk {
@@ -119,6 +119,15 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("linux-ssh-script.tpl", {
+      hostname     = self.public_ip_address,
+      user         = "adminuser",
+      identityfile = "~/.ssh/mtcazurekey"
+    })
+    interpreter = ["bash", "-c"]
   }
 
   tags = {
